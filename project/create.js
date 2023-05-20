7af6273c9755f384aad00c71b9d649d930700c73
KobraRocks <me@kobra.rocks>
2023-05-20 12:57:33 +0200

    light refactor of getPath method

--- project/create.js
+++ project/create.js
@@ -3,8 +3,16 @@ import { isGit, clone } from "../helpers/git.js";
 
 const templateDir = "./project/template";
 
-// bm create project my-project --templateWeb=default-web
-// bm -c project my-project -twd 
+
+/**
+ * Asynchronously creates a new project directory with the given projectName
+ * using the specified projectTemplate. The function copies or clones the
+ * projectTemplate based on whether it is a Git repository or not, respectively.
+ *
+ * @param {string} projectName - The name of the project directory to be created.
+ * @param {string} projectTemplate - The template to be used as the basis of the new project.
+ * @return {Promise<void>} A Promise that resolves once the project directory has been created.
+ */
 export default async function create ( projectName, projectTemplate ) {
 
     const path = getPath( projectTemplate );
@@ -23,13 +31,20 @@ export default async function create ( projectName, projectTemplate ) {
 }
 
 
-function getPath ( option ) {
+/**
+ * Returns the path based on the given option parameter.
+ * ex: option = --template=default-web or --template=/my/templates/directory/my-template
+ *
+ * @param {string} option - The option parameter to extract the path from.
+ * @return {string} The path extracted from the option parameter or a template path if the option is one of the defaults.
+ */
+function getPath( option ) {
+    const path = option.split( "=" )[1];
+    const defaults = ["default-web", "default-desktop", "default-server"];
 
-    const path = option.split("=")[1];
-
-    if ( path === "default-web" || path === "default-desktop" || path === "default-server" )
-        return templateDir + "/" + path;
-
-    return  path;
-
-}
+    if ( defaults.includes(path) ) 
+        return `${templateDir}/${path}`;
+        
+    return path;
+  }
+  
